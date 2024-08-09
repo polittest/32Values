2e1e530915c552833739acb170596f0b76007855
xDMich <149896755+xDMich@users.noreply.github.com>
2024-08-09 19:06:02 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,45 @@
+questions = [
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "freedom": 100,
+            "power": 100,
+            "culture": 0,
+            "inclusivity": 0,
+            "econpower": 0,
+            "econglobe": 0,
+            "globe": 0,
+            "nation": 0,
+            "equity": 0,
+            "horizontal": 0,
+            "planning": 0,
+            "regulation": 0,
+            "automation": 0,
+            "production": 0,
+            "merit": 0,
+            "commonland": 0
+        }
+    },
+
+    {
+        "question": "It is important that we further my group's goals above all others.",
+        "effect": {
+            "freedom": 0,
+            "power": 0,
+            "culture": 0,
+            "inclusivity": 0,
+            "econpower": 0,
+            "econglobe": 0,
+            "globe": 0,
+            "nation": 0,
+            "equity": 0,
+            "horizontal": 0,
+            "planning": 0,
+            "regulation": 0,
+            "automation": 0,
+            "production": 0,
+            "merit": 0,
+            "commonland": 0
+        }
+    }
+];
